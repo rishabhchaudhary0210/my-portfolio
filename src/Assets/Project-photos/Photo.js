ea52b33ba25e0ae4edb2dc7ee088ec1d13f116ad
Rishabh <chaudharyrishabh0210@gmail.com>
2024-01-21 22:25:18 +0530

    added screenshots of projects

---
+++ src/Assets/Project-photos/Photo.js
@@ -0,0 +1,13 @@
+export {default as adminDash} from './../../Assets/Project-photos/admin-dashboard.jpg';
+export {default as dailyJournal} from './../../Assets/Project-photos/daily-journal.jpg';
+export {default as drumKit} from './../../Assets/Project-photos/drum-kit.jpg';
+export {default as foodSite} from './../../Assets/Project-photos/food-site.jpg';
+export {default as groco} from './../../Assets/Project-photos/groco.jpg';
+export {default as marvelPedia} from './../../Assets/Project-photos/marvel-pedia.jpg';
+export {default as simonGame} from './../../Assets/Project-photos/simon-game.jpg';
+export {default as talentConnect} from './../../Assets/Project-photos/talent-connect.jpg';
+export {default as ticTacToe} from './../../Assets/Project-photos/tic-tac-toe.jpg';
+export {default as toDoList} from './../../Assets/Project-photos/to-do-list.jpg';
+
+export {default as laptopFrame} from './../../Assets/Project-photos/laptop-frame.png';
+export {default as mobileFrame} from './../../Assets/Project-photos/mobile-frame.png';
